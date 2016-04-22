20f549bce09896b887341491d51ddc4912fbfea3
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-22 18:47:14 +0300

    Make user preferences applicable

--- nav/nav.js
+++ nav/nav.js
@@ -1,3 +1,11 @@
 angular.module('app').component('nav', {
-  templateUrl: '/nav/nav.html'
+  templateUrl: '/nav/nav.html',
+  controller: function ($firebaseObject, fbRef) {
+    this.userPreferences = $firebaseObject(fbRef.getPreferencesRef());
+
+    this.loaded = false;
+    this.userPreferences.$loaded().then((data) => {
+      this.loaded = true;
+    });
+  }
 });
