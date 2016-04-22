75e01b3c6603f12312e6cb18898b2dcf66c6dc95
Taras Hupalo <taras.hupalo@gmail.com>
2016-04-22 20:13:23 +0300

    Implement expenses removal

--- expenses/expenseListDisplay.js
+++ expenses/expenseListDisplay.js
@@ -2,5 +2,10 @@ angular.module('app').component('expenseListDisplay', {
   templateUrl: '/expenses/expenseListDisplay.html',
   bindings: {
     expenses: '=expenseData'
+  },
+  controller: function () {
+    this.deleteExpense = function(expense) {
+      this.expenses.$remove(expense);
+    }
   }
 });
